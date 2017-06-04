c2937c8b360a919b07360d1f73ddc41886642f52
Michal Orzechowski <michal@orzechowski.info.pl>
2017-06-04 21:13:38 +0200

    initial commit

---
+++ MdbModelGenerator/DbTools.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+namespace MdbModelGenerator
+{
+    public static class DbTools
+    {
+        public static int ReadTable(string query, string conString, ref DataTable dataTable)
+        {
+            var myConnection = new OleDbConnection(conString);
+
+            var adapter = new OleDbDataAdapter(query, myConnection);
+            OleDbCommandBuilder oleDbCommandBuilder = new OleDbCommandBuilder(adapter);
+            adapter.Fill(dataTable);
+
+            return dataTable.Rows.Count;
+        }
+    }
+}
--- MdbModelGenerator/Program.cs
+++ MdbModelGenerator/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MdbModelGenerator
 {
@@ -10,6 +8,50 @@ namespace MdbModelGenerator
     {
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+                return;
+
+            string dbPath = args[0];
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                GetTableFromMdb(dbPath, args[i]);
+            }
+        }
+
+        static void GetTableFromMdb(string dbPath, string tableName)
+        {
+            string ConString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = " + dbPath;
+
+            DataTable dataTable = new DataTable();
+
+            DbTools.ReadTable($"SELECT * FROM {tableName}", ConString, ref dataTable);
+            DataColumnCollection columns = dataTable.Columns;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("using System;");
+            sb.AppendLine();
+
+            sb.AppendLine("namespace MdbModel");
+            sb.AppendLine("{");
+
+            sb.AppendLine($"    public class {tableName}");
+            sb.AppendLine("    {");
+
+            foreach (var item in columns)
+            {
+                var column = columns[item.ToString()];
+
+                sb.AppendLine("        public " + column.DataType.Name + " " + item.ToString() + " { get; set; }");
+            }
+            sb.AppendLine("    }");
+            sb.AppendLine("}");
+
+            using (StreamWriter w = File.CreateText($"{tableName}.cs"))
+            {
+                w.Write(sb.ToString());
+            }
         }
     }
 }
